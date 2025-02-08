5a0ddf9bb8bb25735ffc50380345289ea590ff6f
IWasMouseCop <spencer.dasilva993@gmail.com>
2025-02-07 20:09:56 -0500

    Final

--- Lab2_Group2/Program.py
+++ Lab2_Group2/Program.py
@@ -29,6 +29,7 @@ def search_book(title):
         for row in reader:
             if row[0].lower() == title.lower():
                 result = f'Found: Title: {row[0]}, Author: {row[1]}, Year: {row[2]}'
+                print(result)
                 return result
     return "Book not found"
 
